a6db4f38442e0d24724572fe9577a619ee62cfa0
lukespademan <info@lukespademan.com>
2018-05-29 10:21:27 +0100

    removed unlock indicator

--- i3/lock/lock.sh
+++ i3/lock/lock.sh
@@ -5,6 +5,6 @@ convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 convert ~/.config/i3/lock/lock.png -scale 33% /tmp/lock-icon.png
 convert /tmp/screen.png /tmp/lock-icon.png -gravity center -composite /tmp/screen.png
 #i3lock -u -i /tmp/screen.png
-i3lock -efc 000000 -i /tmp/screen.png
+i3lock -eufc 000000 -i /tmp/screen.png
 
 rm /tmp/screen.png
